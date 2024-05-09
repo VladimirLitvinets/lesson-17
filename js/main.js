6a2df3c4101bb4294a647efb2a4998032e6f2ae9
VladimirLitvinets <Litvinets8802@gmail.com>
2024-05-09 14:26:17 +0300

    finished the scroll menu

--- js/main.js
+++ js/main.js
@@ -1,7 +1,41 @@
-const btn = document.querySelector('.menu__btn');
-const nav = document.querySelector('.header__container');
+const navToggle = document.querySelector('#js-nav-toggle');
+const nav = document.querySelector('#js-nav');
+const documentBody = document.body;
+const navOverlay = document.querySelector('#js-nav-overlay');
+const navItems = document.querySelectorAll('.js-nav-link');
+const sections = document.querySelectorAll('.section[id]');
 
-btn.addEventListener('click', () => {
-	nav.classList.toggle('menu-open')
+navToggle.addEventListener('click', toggleNav);
+
+navOverlay.addEventListener('click', toggleNav);
+
+navItems.forEach((element) => {
+	element.addEventListener('click', toggleNav);
+});
+
+function toggleNav() {
+	nav.classList.toggle('menu_active');
+	documentBody.classList.toggle('no-scroll');
+}
+
+
+const observer = new IntersectionObserver((entries) => {
+	entries.forEach((entrie) => {
+		const entrieElement = entrie.target;
+		const id = entrieElement.id;
+		const navElement = document.querySelector(`.menu__link[href="#${id}"]`);
+
+		if (entrie.isIntersecting) {
+			navElement.classList.add('menu__link_active');
+		} else {
+			navElement.classList.remove('menu__link_active');
+		}
+
+	})
+}, { root: document, rootMargin: "-50% 0px" });
+
+
+sections.forEach((section) => {
+	observer.observe(section);
 });
 
